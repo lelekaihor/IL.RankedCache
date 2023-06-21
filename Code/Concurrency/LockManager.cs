c44178f030edda331d823bec5a155b78c37dd90b
Ihor Leleka <ile@charlietango.dk>
2023-06-21 20:13:07 +0200

    Using a double-checked locking pattern to make sure factory method for creation of lock has been executed only once.

--- Code/Concurrency/LockManager.cs
+++ Code/Concurrency/LockManager.cs
@@ -4,23 +4,25 @@ namespace IL.RankedCache.Concurrency
 {
     internal static class LockManager
     {
-        private static readonly ConcurrentDictionary<string, Lock> Locks = new();
+        private static readonly ConcurrentDictionary<string, Lazy<Lock>> Locks = new();
 
         public static IDisposable GetLock(string key)
         {
-            var concurrentLock = Locks.GetOrAdd(key, _ => new Lock());
+            var lazyLock = Locks.GetOrAdd(key, new Lazy<Lock>(() => new Lock()));
+            var concurrentLock = lazyLock.Value;
             concurrentLock.Wait();
             return concurrentLock;
         }
 
         public static async Task<IDisposable> GetLockAsync(string key)
         {
-            var concurrentLock = Locks.GetOrAdd(key, _ => new Lock());
+            var lazyLock = Locks.GetOrAdd(key, new Lazy<Lock>(() => new Lock()));
+            var concurrentLock = lazyLock.Value;
             await concurrentLock.WaitAsync();
             return concurrentLock;
         }
 
-        private class Lock : IDisposable
+        internal class Lock : IDisposable
         {
             private readonly SemaphoreSlim _semaphoreSlim = new(1);
 
@@ -37,6 +39,11 @@ namespace IL.RankedCache.Concurrency
             public void Dispose()
             {
                 _semaphoreSlim.Release();
+            }
+
+            public int GetState()
+            {
+                return _semaphoreSlim.CurrentCount;
             }
         }
     }
